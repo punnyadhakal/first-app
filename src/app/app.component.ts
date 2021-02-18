dacb1857c56d3eccdcf677e0f3afc5f84f8fb5b7
punnyadhakal <60265806+punnyadhakal@users.noreply.github.com>
2021-02-18 12:31:55 -0700

    this is also a big change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'welcome to the new app';
+  title = 'this is a new andgit  new app';
 }
